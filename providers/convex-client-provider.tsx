ddce91efcbf6e8575e2c852e10ba2f5ae535084b
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-05 21:27:51 +0300

    🔧 fix(convex-client-provider.tsx): create ConvexClientProvider component to wrap the app with ClerkProvider and ConvexProviderWithClerk to provide authentication and ConvexReactClient to the app

---
+++ providers/convex-client-provider.tsx
@@ -0,0 +1,28 @@
+'use client';
+
+import { ClerkProvider, useAuth } from '@clerk/nextjs';
+import {
+  AuthLoading,
+  Authenticated,
+  ConvexProvider,
+  ConvexReactClient
+} from 'convex/react';
+import { ConvexProviderWithClerk } from 'convex/react-clerk';
+
+interface ConvexClientProviderProps {
+  children: React.ReactNode;
+}
+
+const convexUrl = process.env.NEXT_PUBLIC_CONVEX_URL!;
+
+const convex = new ConvexReactClient(convexUrl);
+
+export function ConvexClientProvider({ children }: ConvexClientProviderProps) {
+  return (
+    <ClerkProvider>
+      <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
+        {children}
+      </ConvexProviderWithClerk>
+    </ClerkProvider>
+  );
+}
