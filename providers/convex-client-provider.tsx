7a8a69053d6906fc3e40ed616d0db79a14f3d74c
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-06 10:29:34 +0300

    🎨 style(globals.css): add missing whitespace and line breaks for better readability and maintainability
    🆕 feat(auth/loading.tsx): add Loading component to display a loading animation with a logo
    
    🔧 refactor(convex-client-provider.tsx): import Loading component and use it inside AuthLoading component for a better user experience

--- providers/convex-client-provider.tsx
+++ providers/convex-client-provider.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Loading } from '@/components/auth/loading';
 import { ClerkProvider, useAuth } from '@clerk/nextjs';
 import {
   AuthLoading,
@@ -21,7 +22,10 @@ export function ConvexClientProvider({ children }: ConvexClientProviderProps) {
   return (
     <ClerkProvider>
       <ConvexProviderWithClerk useAuth={useAuth} client={convex}>
-        {children}
+        <Authenticated>{children}</Authenticated>
+        <AuthLoading>
+          <Loading />
+        </AuthLoading>
       </ConvexProviderWithClerk>
     </ClerkProvider>
   );
