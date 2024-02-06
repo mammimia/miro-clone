f6c99c3e6eda768def393c96f79abd68a0d89080
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-06 12:31:37 +0300

    🎨 style(navbar/index.tsx): improve readability and add user button to navbar

--- app/(dashboard)/_components/navbar/index.tsx
+++ app/(dashboard)/_components/navbar/index.tsx
@@ -1,7 +1,12 @@
 'use client';
 
+import { UserButton } from '@clerk/nextjs';
+
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 bg-green-500 p-5">Navbar</div>
+    <div className="flex items-center gap-x-4 bg-green-500 p-5">
+      <div className="hidden bg-yellow-500 lg:flex lg:flex-1">Search</div>
+      <UserButton />
+    </div>
   );
 };
