52e0ef6d88b71f67942723b361ab2c34c8159473
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-06 12:20:41 +0300

    ✨ feat(sidebar): add Sidebar component to the dashboard
    The Sidebar component is a fixed sidebar that displays on the left side of the dashboard. It has a width of 60 pixels and a dark blue background color. The component is responsible for displaying navigation links and other relevant information.

---
+++ app/(dashboard)/_components/sidebar/index.tsx
@@ -0,0 +1,7 @@
+export const Sidebar = () => {
+  return (
+    <aside className="fixed left-0 z-[1] flex h-full w-[60px] flex-col gap-y-4 bg-blue-950 p-3 text-white">
+      Side
+    </aside>
+  );
+};
