64281d9d73973f9ced8f924b08d8054953cde125
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-07 14:47:22 +0300

    🎨 style(org-sidebar): improve styling and layout of the organization sidebar component 🔧 chore(org-sidebar): update font import to use Poppins font with specific subsets and weight 🔧 chore(org-sidebar): update image import to use next/image for better performance ✨ feat(org-sidebar): add link to home page with logo and board name in the organization sidebar ✨ feat(org-sidebar): add organization switcher component with custom appearance styling

--- app/(dashboard)/_components/org-sidebar/index.tsx
+++ app/(dashboard)/_components/org-sidebar/index.tsx
@@ -1,9 +1,48 @@
 'use client';
 
+import { cn } from '@/lib/utils';
+import { OrganizationSwitcher } from '@clerk/nextjs';
+import { Poppins } from 'next/font/google';
+import Image from 'next/image';
+import Link from 'next/link';
+
+const font = Poppins({
+  subsets: ['latin'],
+  weight: ['600']
+});
+
 export const OrgSidebar = () => {
   return (
-    <div className="hidden w-[206px] flex-col space-y-6 bg-red-500 pl-5 pt-5 text-white lg:flex">
-      OrgSidebar
+    <div className="hidden w-[206px] flex-col space-y-6 pl-5 pt-5 lg:flex">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image src="/logo.svg" alt="Logo" height={60} width={60} />
+          <span className={cn('text-2xl font-semibold', font.className)}>
+            Board
+          </span>
+        </div>
+      </Link>
+      <OrganizationSwitcher
+        hidePersonal
+        appearance={{
+          elements: {
+            rootBox: {
+              display: 'flex',
+              justifyContent: 'center',
+              alignItems: 'center',
+              width: '100%'
+            },
+            organizationSwitcherTrigger: {
+              padding: '6px',
+              width: '100%',
+              borderRadius: '8px',
+              border: '1px solid #E5E7EB',
+              justifyContent: 'space-between',
+              backgroundColor: 'white'
+            }
+          }
+        }}
+      />
     </div>
   );
 };
