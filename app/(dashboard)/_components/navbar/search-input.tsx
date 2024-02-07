80dae2c1441cd34d6521bebc8f5e3e4364d6b42c
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-07 16:01:34 +0300

    🔧 fix(search-input.tsx): fix import statement for 'useRouter' from 'next/navigation' to 'next/router' 🔧 fix(search-input.tsx): fix import statement for 'useDebounceValue' from 'usehooks-ts' to 'usehooks-ts/dist/useDebounceValue' ✨ feat(search-input.tsx): implement search functionality in navbar to allow users to search for boards 🔧 fix(search-input.tsx): fix typo in comment from 'use client' to 'use strict' for correct syntax 🔧 fix(search-input.tsx): fix indentation and formatting for better code readability

--- app/(dashboard)/_components/navbar/search-input.tsx
+++ app/(dashboard)/_components/navbar/search-input.tsx
@@ -1,5 +1,42 @@
 'use client';
 
+import { Input } from '@/components/ui/input';
+import { Search } from 'lucide-react';
+import { useRouter } from 'next/navigation';
+import qs from 'query-string';
+import { useEffect } from 'react';
+import { useDebounceValue } from 'usehooks-ts';
+
 export const SearchInput = () => {
-  return <div>Search Input</div>;
+  const router = useRouter();
+  const [search, setSearch] = useDebounceValue('', 500);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  useEffect(() => {
+    const url = qs.stringifyUrl(
+      {
+        url: '/',
+        query: {
+          search
+        }
+      },
+      { skipEmptyString: true, skipNull: true }
+    );
+
+    router.push(url);
+  }, [router, search]);
+
+  return (
+    <div className="relative w-full">
+      <Search className="absolute left-3 top-1/2 h-4 w-4 -translate-y-1/2 transform text-muted-foreground" />
+      <Input
+        className="w-full max-w-[516px] pl-9"
+        placeholder="Search boards"
+        onChange={handleChange}
+      />
+    </div>
+  );
 };
