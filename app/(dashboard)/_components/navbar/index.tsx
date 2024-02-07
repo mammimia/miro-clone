cce98db9238ba535aef2d635e3b03273726fcb9a
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-07 15:42:47 +0300

    🎨 style(navbar): remove unnecessary background color from navbar component 🚀 feat(navbar): add search input component to navbar for future search functionality

--- app/(dashboard)/_components/navbar/index.tsx
+++ app/(dashboard)/_components/navbar/index.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import { UserButton } from '@clerk/nextjs';
+import { SearchInput } from './search-input';
 
 export const Navbar = () => {
   return (
-    <div className="flex items-center gap-x-4 bg-green-500 p-5">
-      <div className="hidden bg-yellow-500 lg:flex lg:flex-1">Search</div>
+    <div className="flex items-center gap-x-4 p-5">
+      <div className="hidden lg:flex lg:flex-1">
+        <SearchInput />
+      </div>
       <UserButton />
     </div>
   );
