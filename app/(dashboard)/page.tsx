c71ac002cbc4c6718279970df9544decabbeaba3
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-08 15:31:50 +0300

    🔧 chore(page.tsx): refactor DashboardPage component to use conditional rendering based on organization existence 🚀 feat(page.tsx): add BoardList component to render when organization exists, passing orgId and query props

--- app/(dashboard)/page.tsx
+++ app/(dashboard)/page.tsx
@@ -1,9 +1,25 @@
+'use client';
+import { useOrganization } from '@clerk/nextjs';
 import { EmptyOrg } from './_components/empty-org';
+import { BoardList } from './_components/board-list';
 
-const DashboardPage = () => {
+interface DashboardPageProps {
+  searchParams: {
+    search?: string;
+    favorites?: boolean;
+  };
+}
+
+const DashboardPage = ({ searchParams }: DashboardPageProps) => {
+  const { organization } = useOrganization();
+
   return (
     <div className="h-[calc(100%-80px)] flex-1 p-6">
-      <EmptyOrg />
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 };
