17961198aff17b8f306aa2b2b8014b90fd42ea56
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-09 20:12:37 +0300

    🚀 feat(board-list.tsx): refactor BoardList component to use separate components for empty boards, favorites, and search results for better code organization and reusability

--- app/(dashboard)/_components/board-list.tsx
+++ app/(dashboard)/_components/board-list.tsx
@@ -1,5 +1,9 @@
 'use client';
 
+import EmptyBoards from './sidebar/empty-boards';
+import EmptyFavorites from './sidebar/empty-favorites';
+import EmptySearch from './sidebar/empty-search';
+
 interface BoardListProps {
   orgId: string;
   query: {
@@ -12,15 +16,15 @@ export const BoardList = ({ orgId, query }: BoardListProps) => {
   const data = []; // Call API to get data
 
   if (!data?.length && query.search) {
-    return <div>Try searching for something else</div>;
+    return <EmptySearch />;
   }
 
   if (!data?.length && query.favorites) {
-    return <div>No favorites</div>;
+    return <EmptyFavorites />;
   }
 
   if (!data?.length) {
-    return <div>No boards</div>;
+    return <EmptyBoards />;
   }
 
   return <div>{JSON.stringify(query)}</div>;
