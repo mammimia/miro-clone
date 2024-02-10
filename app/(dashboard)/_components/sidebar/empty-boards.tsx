d9bb8183edc4d1b04707b6184ea56efd81a571a6
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-10 21:24:12 +0300

    🔧 fix(empty-boards.tsx): add functionality to create a board for the organization when 'Create Board' button is clicked

--- app/(dashboard)/_components/sidebar/empty-boards.tsx
+++ app/(dashboard)/_components/sidebar/empty-boards.tsx
@@ -1,8 +1,22 @@
+'use client';
+
 import { Button } from '@/components/ui/button';
+import { api } from '@/convex/_generated/api';
+import { useOrganization } from '@clerk/nextjs';
+import { useMutation } from 'convex/react';
 import Image from 'next/image';
 import React from 'react';
 
 const EmptyBoards = () => {
+  const { organization } = useOrganization();
+  const create = useMutation(api.board.create);
+
+  const handleClick = () => {
+    if (!organization) return;
+
+    create({ orgId: organization.id, title: 'My first board' });
+  };
+
   return (
     <div className="flex h-full flex-col items-center justify-center">
       <Image src="/note.svg" height={200} width={200} alt="Empty" />
@@ -11,7 +25,9 @@ const EmptyBoards = () => {
         Start by creating a board for your organization
       </p>
       <div className="mt-6">
-        <Button size="lg">Create Board</Button>
+        <Button size="lg" onClick={handleClick}>
+          Create Board
+        </Button>
       </div>
     </div>
   );
