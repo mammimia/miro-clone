b08b32b59d9029fe27ede59b480d7cbead6f3dea
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-12 19:12:44 +0300

    🎨 style(board-footer.tsx): improve UI by adding a star button for favoriting boards

--- app/(dashboard)/_components/board-card/board-footer.tsx
+++ app/(dashboard)/_components/board-card/board-footer.tsx
@@ -24,6 +24,18 @@ export const BoardFooter = ({
       <p className="truncate text-[11px] text-muted-foreground opacity-0 transition-opacity group-hover:opacity-100">
         Created by {authorLabel}, {createdAtLabel}
       </p>
+      <button
+        disabled={disabled}
+        onClick={onClick}
+        className={cn(
+          'absolute right-3 top-3 text-muted-foreground opacity-0 transition hover:text-blue-600 group-hover:opacity-100',
+          disabled && 'cursor-not-allowed opacity-75'
+        )}
+      >
+        <Star
+          className={cn('h-4 w-4', isFavorite && 'fill-blue-600 text-blue-600')}
+        />
+      </button>
     </div>
   );
 };
