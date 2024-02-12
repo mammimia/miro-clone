67ec1dc84cf1af3b853959c4dcb7c8bc6e9d747d
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-12 19:36:25 +0300

    🚀 feat(board-card): add authorLabel and createdAtLabel to display author and creation time in BoardCard

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,7 +1,11 @@
 'use client';
 
+import { useAuth } from '@clerk/nextjs';
+import { formatDistanceToNow } from 'date-fns';
 import Image from 'next/image';
 import Link from 'next/link';
+import { BoardFooter } from './board-footer';
+import { Overlay } from './overlay';
 
 interface BoardCardProps {
   id: string;
@@ -24,12 +28,25 @@ export const BoardCard = ({
   orgId,
   isFavorite
 }: BoardCardProps) => {
+  const { userId } = useAuth();
+  const authorLabel = authorId === userId ? 'you' : authorName;
+  const createdAtLabel = formatDistanceToNow(createdAt, { addSuffix: true });
+
   return (
     <Link href={`/board/${id}`}>
       <div className="group flex aspect-[100/127] flex-col justify-between overflow-hidden rounded-lg border">
         <div className="relative flex-1 bg-amber-50">
           <Image src={imageUrl} alt={title} fill className="object-fit" />
+          <Overlay />
         </div>
+        <BoardFooter
+          isFavorite={isFavorite}
+          title={title}
+          authorLabel={authorLabel}
+          createdAtLabel={createdAtLabel}
+          onClick={() => {}}
+          disabled={false}
+        />
       </div>
     </Link>
   );
