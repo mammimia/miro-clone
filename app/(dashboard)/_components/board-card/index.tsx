e8316f0e45f3f62047beec761eb1d7fcb4ef1f24
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-12 18:11:58 +0300

    ✨ feat(board-card): add BoardCard component to display board information with image and link to board details page

---
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -0,0 +1,36 @@
+'use client';
+
+import Image from 'next/image';
+import Link from 'next/link';
+
+interface BoardCardProps {
+  id: string;
+  title: string;
+  imageUrl: string;
+  authorId: string;
+  authorName: string;
+  createdAt: number;
+  orgId: string;
+  isFavorite: boolean;
+}
+
+export const BoardCard = ({
+  id,
+  title,
+  imageUrl,
+  authorId,
+  authorName,
+  createdAt,
+  orgId,
+  isFavorite
+}: BoardCardProps) => {
+  return (
+    <Link href={`/board/${id}`}>
+      <div className="group flex aspect-[100/127] flex-col justify-between overflow-hidden rounded-lg border">
+        <div className="relative flex-1 bg-amber-50">
+          <Image src={imageUrl} alt="Doodle" fill className="object-fit" />
+        </div>
+      </div>
+    </Link>
+  );
+};
