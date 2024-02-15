95a32e1e05b907320a6f6f686d41e4ad8c110955
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-15 20:23:08 +0300

    🚀 feat(actions.tsx): add functionality to copy board link to clipboard when clicked to improve user experience

--- components/actions.tsx
+++ components/actions.tsx
@@ -8,6 +8,8 @@ import {
   DropdownMenuSeparator,
   DropdownMenuTrigger
 } from '@/components/ui/dropdown-menu';
+import { Link2 } from 'lucide-react';
+import { toast } from 'sonner';
 
 interface ActionsProps {
   id: string;
@@ -24,9 +26,34 @@ export const Actions = ({
   side,
   sideOffset
 }: ActionsProps) => {
+  const handleCopyLink = () => {
+    navigator.clipboard
+      .writeText(`${window.location.origin}/board/${id}`)
+      .then(() => {
+        toast.success('Link copied to clipboard');
+      })
+      .catch(() => {
+        toast.error('Failed to copy link to clipboard');
+      });
+  };
+
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>{children}</DropdownMenuTrigger>
+      <DropdownMenuContent
+        side={side}
+        sideOffset={sideOffset}
+        className="w-60"
+        onClick={(event) => event.stopPropagation()}
+      >
+        <DropdownMenuItem
+          className="cursor-pointer p-3"
+          onClick={handleCopyLink}
+        >
+          <Link2 className="mr-2 h-4 w-4" />
+          Copy board link
+        </DropdownMenuItem>
+      </DropdownMenuContent>
     </DropdownMenu>
   );
 };
