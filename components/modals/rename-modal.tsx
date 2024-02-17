7be0f1e792009618b4a35c8c8ca2ffa1226ed89d
Muhammed Ali AYDIN <muhammedaliaydin97@gmail.com>
2024-02-17 19:22:32 +0300

    📝 refactor(rename-modal.tsx): Add Input component for user to enter a new title for the board and implement handleSubmit function for saving the new title

--- components/modals/rename-modal.tsx
+++ components/modals/rename-modal.tsx
@@ -1,6 +1,8 @@
 'use client';
 
 import { useRenameModal } from '@/store/use-rename-modal';
+import { useEffect, useState } from 'react';
+import { Button } from '../ui/button';
 import {
   Dialog,
   DialogClose,
@@ -10,9 +12,17 @@ import {
   DialogHeader,
   DialogTitle
 } from '../ui/dialog';
+import { Input } from '../ui/input';
 
 export const RenameModal = () => {
   const { isOpen, initialValues, onClose } = useRenameModal();
+  const [title, setTitle] = useState(initialValues.title);
+
+  useEffect(() => {
+    setTitle(initialValues.title);
+  }, [initialValues.title]);
+
+  const handleSubmit = () => {};
 
   return (
     <Dialog open={isOpen} onOpenChange={onClose}>
@@ -20,8 +30,28 @@ export const RenameModal = () => {
         <DialogHeader>
           <DialogTitle>Edit board title</DialogTitle>
         </DialogHeader>
+        <DialogDescription>Enter a new title for this board</DialogDescription>
+        <form onSubmit={handleSubmit} className="space-y-4">
+          <Input
+            disabled={false}
+            required
+            maxLength={60}
+            value={title}
+            onChange={(e) => setTitle(e.target.value)}
+            placeholder="Board title"
+          />
+          <DialogFooter>
+            <DialogClose asChild>
+              <Button type="button" variant="outline">
+                Cancel
+              </Button>
+            </DialogClose>
+            <Button disabled={false} type="submit">
+              Save
+            </Button>
+          </DialogFooter>
+        </form>
       </DialogContent>
-      <DialogDescription>Enter a new title for this board</DialogDescription>
     </Dialog>
   );
 };
